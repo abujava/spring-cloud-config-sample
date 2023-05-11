f48ee07d67f06ec8dda8d6d18b86353bc99d945d
Muhammad <admin@abujava.com>
2023-05-11 11:04:30 +0500

    Add auth-service

---
+++ auth-service/src/main/java/com/abujava/authservice/Startup.java
@@ -0,0 +1,26 @@
+package com.abujava.authservice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+/**
+ * This class is not documented :(
+ *
+ * @author Muhammad Muminov
+ * @since 5/11/2023
+ */
+@Component
+public class Startup implements CommandLineRunner {
+    private final Environment environment;
+
+    public Startup(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String value = environment.getProperty("app.user.token-expiration-in-ms");
+        System.out.println(value);
+    }
+}
